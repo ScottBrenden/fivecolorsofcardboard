2ebff638a48d2b0be48fd10b6fbfef1a0cbb655a
ScottBrenden <scott.b.brenden@gmail.com>
2018-06-06 18:48:39 -0700

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict'
 
 const express = require('express')
-//const morgan = require('morgan')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('common'))
+app.use(express.static(`${__dirname}/build`))
+app.get('*', (req,res) => res.sendFile(`${__dirname}/build/index.html`))
+
+app.listen(process.env.PORT, () => {
+  console.log('Server running on port:', process.env.PORT);
+})
